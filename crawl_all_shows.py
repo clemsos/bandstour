a6e480d0a7f2643a2e23331534ef0ae7a1a80326
Clément Renaud <clement.renaud@gmail.com>
2015-06-15 22:10:39 +0200

    bulletproof crawling

--- crawl_all_shows.py
+++ crawl_all_shows.py
@@ -1,7 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import json
 import urllib
 import time
 import csv
+import math
+import os
 
 PLACE_BASE_URL = "https://app.bandsintown.com/events/popular/?location=PLACE&radius=10&per_page=100&authenticate=false&page=PAGE_NUMBER"
 
@@ -16,8 +21,17 @@ def get_page_url(place, page_number):
     return url.replace("PAGE_NUMBER", str(page_number))
 
 def get_page(place, page_number):
-    url = get_page_url(place, page_number)
-    urllib.urlretrieve( url , filename=get_filename(place, page_number))
+
+    f = get_filename(place, page_number)
+
+    # check if the file already exists
+    if not os.path.isfile(f):  
+        print f, " downloading"
+        url = get_page_url(place, page_number)
+        urllib.urlretrieve( url , filename=f)
+    else :
+        print f, " already exists"
+
 
 def get_dates_from_place(place):
 
@@ -27,12 +41,13 @@ def get_dates_from_place(place):
     # compute number of pages
     with open( get_filename(place, 1) ) as data_file:    
         page = json.load(data_file)
-        nb_of_pages = round( int(page["pages"]["total_results"]) / int(page["pages"]["results_per_page"]) )
+        nb_of_pages =  math.ceil( int(page["pages"]["total_results"]) / int(page["pages"]["results_per_page"]) ) +1
+        print "  total", place, nb_of_pages
 
     # crawl api
-    for page_number in range(2, int(nb_of_pages)) :
+    for page_number in range(2, int(nb_of_pages)+1) :
         time.sleep(1)
-        print place, str(page_number), "/", str(nb_of_pages)
+        # print place, str(page_number), "/", str(nb_of_pages)
         get_page(place,  page_number)
 
 # places = ["London%2C+UK", "Lyon%2C+France", "Paris%2C+France"]
@@ -44,6 +59,8 @@ with open('list_villes/sousprefs') as souspref_file:
 with open('list_villes/prefs') as pref_file:
     places += pref_file.read().splitlines()
 
+# places = ["Saint-Jean-d’Angély"] # reproduce error
+
 for place in places :
     if place != "":  place.replace(' ',"%20")
     place = place+"%2C+France"
@@ -51,7 +68,7 @@ for place in places :
     try :
         get_dates_from_place(place)
     except :
-        pass
+        print "ERROR :",  place
 
 # with open('data/london.json') as data_file:    
 #     data = json.load(data_file)
