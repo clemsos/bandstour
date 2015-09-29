b1f53e2c4716922e5762e19fbbb0668365ba27a8
Makio135 <makio135@gmail.com>
2015-09-29 14:50:53 +0200

    materialize + autocomplete

--- visualizer/client/searchbox/searchbox.js
+++ visualizer/client/searchbox/searchbox.js
@@ -1,35 +1,62 @@
-Template.searchbox.rendered = function() {
-    Meteor.typeahead.inject();
-};
+/*Template.searchbox.rendered = function( ) {
+    Meteor.typeahead.inject( );
+    Meteor.typeahead( 'input#search', function( ) {
 
-Template.searchbox.helpers({
-    lookupArtists : function(query, sync, callback) {
-        Meteor.call('search', query, {}, function(err, res) {
-            if (err) {
-                console.log(err);
+    } )
+};*/
+
+/*Template.searchbox.helpers( {
+    lookupArtists: function( query, sync, callback ) {
+        Meteor.call( 'search', query, {}, function( err, res ) {
+            if ( err ) {
+                console.log( err );
                 return;
             }
-            callback(res.map(function(v){ return {value: v.name}; }));
-        });
+            callback( res.map( function( v ) {
+                return {
+                    value: v.name
+                };
+            } ) );
+        } );
     }
-});
+} );*/
 
-Template.searchbox.events({
-    'submit form': function (e, template) {
-            e.preventDefault();
-            var bandName = event.target.bandName.value;
-            if( !bandName ) return;
-            Router.go('/artists/' + slugify(bandName));
-     }
-});
+Template.searchbox.events( {
+    'submit form': function( e, template ) {
+        e.preventDefault( );
+        var bandName = event.target.bandName.value;
+        if ( !bandName ) return;
+        Router.go( '/artists/' + slugify( bandName ) );
+    },
 
+    'click .mdi-navigation-close': function( e ) {
+        e.preventDefault( );
+        document.getElementById("search").value = '';
+    },
 
-function slugify(text)
-{
-  return text.toString().toLowerCase()
-    .replace(/\s+/g, '-')           // Replace spaces with -
-    .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
-    .replace(/\-\-+/g, '-')         // Replace multiple - with single -
-    .replace(/^-+/, '')             // Trim - from start of text
-    .replace(/-+$/, '');            // Trim - from end of text
+    'keyup #search': function( e ) {
+        console.log( event.target.value );
+        if( event.target.value != ''){
+            Meteor.call( 'search', slugify( event.target.value ), {}, function( err, res ) {
+                if ( err ) {
+                    console.log( err );
+                    return;
+                }
+                console.log( res.map( function( v ) {
+                    return {
+                        value: v.name
+                    };
+                } ) );
+            } );
+        }
+    }
+} );
+
+function slugify( text ) {
+    return text.toString( ).toLowerCase( )
+        .replace( /\s+/g, '-' ) // Replace spaces with -
+        .replace( /[^\w\-]+/g, '' ) // Remove all non-word chars
+        .replace( /\-\-+/g, '-' ) // Replace multiple - with single -
+        .replace( /^-+/, '' ) // Trim - from start of text
+        .replace( /-+$/, '' ); // Trim - from end of text
 }
--- visualizer/server/methods/Artists.js
+++ visualizer/server/methods/Artists.js
@@ -1,20 +1,26 @@
-Meteor.methods({
-    getGigsByArtist : function(artistName) {
-         return Artists.findOne({ _id : artistName });
+Meteor.methods( {
+    getGigsByArtist: function( artistName ) {
+        return Artists.findOne( {
+            _id: artistName
+        } );
     },
-    search : function(query, options) {
+
+    search: function( query, options ) {
         options = options || {};
 
         // guard against client-side DOS: hard limit to 50
-        if (options.limit) {
-            options.limit = Math.min(50, Math.abs(options.limit));
+        if ( options.limit ) {
+            options.limit = Math.min( 50, Math.abs( options.limit ) );
         } else {
             options.limit = 50;
         }
 
         // TODO fix regexp to support multiple tokens
-        var regex = new RegExp("^" + query);
-        return Artists.find({name: {$regex:  regex}}, options).fetch();
-
+        var regex = new RegExp( "^" + query );
+        return Artists.find( {
+            slug: {
+                $regex: regex
+            }
+        }, options ).fetch( );
     }
-});
+} );
