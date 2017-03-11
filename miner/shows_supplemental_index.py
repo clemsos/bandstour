9021d7eec385897b609aa20b9cdf64138b0ddae0
ungentilgarçon <gregory.bahde@laposte.net>
2017-03-11 15:25:14 +0100

    new datas

--- miner/shows_supplemental_index.py
+++ miner/shows_supplemental_index.py
@@ -7,7 +7,6 @@ from datetime import datetime
 from pymongo import MongoClient
 from collections import defaultdict
 from dateutil import parser
-
 # setup mongo
 client = MongoClient()
 db = client["bandstour"]
@@ -22,32 +21,33 @@ countNAME = 0
 count = 0
 # date parsing
 DATETIME_FORMAT = '%Y-%m-%dT%H:%M:%S'
-DATETIME_FORMAT_FOR_MSBNS ='%Y-%m-%d'
-#retrieve label for each artist before a tour starts
+DATETIME_FORMAT_FOR_MSBNZ ='%Y-%m-%d'
+# init label_db_retrieval
 labels_aliases ={}
+#retrieve label for each artist before a tour starts
+#i.e what is the album previously to the first year
 for artist in db.minedArtists.find() :
+                    print artist["name"]
+    
                     #print db.minedArtists[artist]
                     #print artist
                     labels_dict= defaultdict(list)
                     labels_dict["id"]=[]
 
-                    if  artist["mbid"] is None:
+                    if  artist["mbid"] is ( None ) :
                         print ("NOMBID for",artist["name"])
 
-    #                     try :
-    #                         print(musicbrainzngs.browse_releases( artist["name"], includes=["labels"] ))
-    #                     except musicbrainzngs.musicbrainz.ResponseError as e:
-    # # catastrophic error. bail.
-    #                         print e
-    #                         #sys.exit(1)
-
-
                     else:
+                     if  bool(artist["mbid"]) is ( False ) :
+                      print ("NOMBID2 for",artist["name"])
 
+                     else:
+                      print "aartist_mbid", artist["mbid"]
+                      try:
                         artist["releases"]=(musicbrainzngs.browse_releases( artist["mbid"], release_type=["album"], includes=["labels"] ))
-                        #artist["release_groups"]=(musicbrainzngs.browse_release_groups( artist["mbid"],release_type=["album"], includes=["tags"]))
-                        #print "artist",artist
-                        print "...."
+                            
+                                  
+                        #print "...."
                         #print artist["release_groups"]
                         key= 0
                         for release in  artist["releases"]["release-list"]:
@@ -57,100 +57,51 @@ for artist in db.minedArtists.find() :
                                 #print "albumtitle:",release["title"]
                                 #print "albumid:",release["id"]
                                 #print "label-list",release["label-info-list"]
-                                for label in release["label-info-list"]:
+                                print "release",release
+                                if 'label' in release["label-info-list"]:
+                                 for label in release["label-info-list"]:
+                                    #WE NEED TO TEST HERE
                                     #print "label:",label["label"]["sort-name"]
                                     #print "labelid:",label["label"]["id"]
-
+#WE CREATE A DICTIONNARY WITH OUR FIELDS, WITH FORMAT
                                     if label["label"]["id"] not in labels_dict["labids"]:
                                         labels_dict["labids"].append(label["label"]["id"])
                                         labels_dict[label["label"]["id"]]={}
 
                                         labels_dict[label["label"]["id"]]["label_name"]=label["label"]["sort-name"]
                                         labels_dict[label["label"]["id"]]["label_id" ]=label["label"]["id"]
-                                        #WE Build the label aliases tables here so that all labels have their relationships##
+#WE Build the label aliases tables here so that all labels have their relationships##
                                         if label["label"]["id"] not in labels_aliases:
                                             labels_aliases[label["label"]["id"]] = musicbrainzngs.get_label_by_id(label["label"]["id"], includes=["aliases", "annotation", "area-rels", "label-rels", "place-rels"])
                                         #print "labels_aliases",labels_aliases
-                                        # labels_dict[key].append(label["label"]["id"])
-                                        # artist["labels"]["name"]=label["label"]["sort-name"]
-                                #if     release["id"] not in artist["labels"]["titles"]:
-
-
-                                    labels_dict[label["label"]["id"]][key]={}
-                                    print key
-                                    labels_dict[label["label"]["id"]][key]["album_id"]=release["id"]
-                                    labels_dict[label["label"]["id"]][key]["album_title"]=release["title"]
-                                    # artist["labels"]["titles"]=release["id"]
-                                    # artist["labels"]["titles"]["name"]=release["title"]
+                                    labels_dict[label["label"]["id"]][str(key)]={}
+                                    #print key
+                                    labels_dict[label["label"]["id"]][str(key)]["album_id"]=release["id"]
+                                    labels_dict[label["label"]["id"]][str(key)]["album_title"]=release["title"]
                                     releDate=release["release-event-list"][0]
                                 #print "releDate", release["release-event-list"][0]
                                     if  releDate.has_key("date"):
                                         dateRel=arrow.get(releDate["date"])
-                                        print "date:", dateRel.strftime('%Y-%m-%d')
-                                    # artist["labels"]["titles"]["date"]=dateRel.strftime('%Y-%m-%d')
-                                    ##labels_dict[label["label"]["id"][key]].append({"album_date":dateRel.strftime('%Y-%m-%d')})
-                                        labels_dict[label["label"]["id"]][key]["album_date"]=dateRel.strftime('%Y-%m-%d')
+                                        #print "date:", dateRel.strftime('%Y-%m-%d')
+                                        labels_dict[label["label"]["id"]][str(key)]["album_date"]=dateRel.strftime('%Y-%m-%d')
                                     key = key +1
-                                    #NOW we make a nice lightweight object to push to mongo
-
-                                else:
-                                    print "NORELEASEDATE2"
+                                #else:
+                                    #print "NORELEASEDATE2"
                                     #key = key +1
-                            else:
+                            #else:
                                 #key = key +1
-                                print "NORELEASEDATE"
+                                #print "NORELEASEDATE"
+#NOW we make a nice lightweight object to push to mongo
+                        labels_dict["name"]=artist["name"]
+                        print labels_dict,'\r\n'
+                        db.labels_dict_per_artist.update({'myid': artist["name"] } ,labels_dict, upsert=True, multi=False)
+                      except  musicbrainzngs.ResponseError:
+                        print "oups",artist["name"]
 
-                            print labels_dict
-                            db.minedArtists.artist.insert_one(labels_dict)
-                            print labels_aliases
-                            # print ("title",release["title"])
-                            # for date in release["release-event-list"]:
-                            #     print date["date"]
-                            # # print ("date", release['release-event-list']["date"])
-                            # print "...."
-                            # for label in release["label-info-list"]:
-                            #     print "label",label["label"]["sort-name"]
-                            #     print "...."
-                            #     print "labelid",label["label"]["id"]
-                    #,date=[ mindatedisc TO  tourBegining ]
-                    #     result = musicbrainzngs.
-#                        art = disc.artist( artist["mbid"])
-#                        print art
-                    #countMBID = countMBID +1
-#count = count +1
-
-        # city  = lookup_city( show["venue"]["city"] )
-        # venue = lookup_venue( show["venue"]["city"] )
-        # show = lookup_show( show["datetime"], show["artists"] )
-
-#                 for tour in artist["tours"] :
-#
-#                     # date parsing
-#                     tourBegining = tour["gigs"][0]["datetime"].strftime('%Y-%m-%d')
-#                     print tourBegining
-#                     arw = arrow.get(tourBegining)
-#                     #min date time for a record:6months before tour)
-#                     mindatedisc = arw.replace(months=-6).strftime('%Y-%m-%d')
-#                     print mindatedisc
-#
-#
-# print count
-# print countMBID
-# print countNAME
-
-
-# def lookup_venue( venue ):
-#     # look for venue
-#     if venue not exists :
-#         create venue
-#     else :
-#         return venue
-
-# def lookup_city( city ):
-#         if city not exists :
-#             create_city
-#         else :
-#             return city
-
-# def lookup_show(date, artists):
-#     return
+                #with open(os.path.join(DATA_FOLDER,artist["name"],".json"), 'w') as outfile:
+                             #   json.dump(labels_dict, outfile)
+                            #db.minedArtists.artist.insert_one(labels_dict)
+#with open(os.path.join(DATA_FOLDER,"label_equiv_table.json"), 'w') as outfile:
+ #                               json.dump(labels_aliases, outfile)
+print labels_aliases,'\r\n\r\n\r\n'
+  
