1dbc729786a92f2dd0df0f56a76db694fad16792
ungentilgarcon <monsieur.goonie@gmail.com>
2017-06-02 00:24:47 +0200

    progressingtoward JSON LN integration,file is parsed,  we need a method to change nodecolor according to LN or not

---
+++ visualizer/server/start.js
@@ -0,0 +1,11 @@
+//https://stackoverflow.com/questions/22004412/how-to-use-user-json-file-in-meteor FOR PARSING A JSON
+
+
+
+//https://stackoverflow.com/questions/24474675/meteor-reading-simple-json-file
+// //https://stackoverflow.com/questions/24474675/meteor-reading-simple-json-file
+// Meteor.methods({ readit: function(){
+// console.log("READIT CALLLED TO THE RESCUE",arv);
+//   return arv;
+//   }
+// })
